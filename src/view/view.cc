26b013139081a7dd6a72e1e155e08012a5242fb7
violator <violator@student.21-school.ru>
2023-03-16 16:00:39 +0300

    plot view

--- src/view/view.cc
+++ src/view/view.cc
@@ -17,3 +17,30 @@ void MainWindow::on_calcculate_clicked() {
   }
 
 }
+
+
+void MainWindow::on_plot_clicked() {
+
+  ui->widget->clearGraphs();
+  h = ui->lineEdit_step->text().toDouble();
+
+  DataPlot data_plot_input(ui->text_label->text().toStdString(),
+                           ui->lineEdit_x_min->text().toDouble(),
+                           ui->lineEdit_x_max->text().toDouble() + h);
+  std::pair<QVector<double>, QVector<double>> data_plot;
+
+  ui->widget->xAxis->setRange(data_plot_input.x_begin_, data_plot_input.x_end_);
+  ui->widget->yAxis->setRange(ui->lineEdit_y_min->text().toDouble(), ui->lineEdit_y_max->text().toDouble());
+  try {
+    data_plot = controller->PlotCalculation(data_plot_input);
+    ui->widget->addGraph();
+    ui->widget->graph(0)->addData(data_plot.first, data_plot.second);
+    ui->widget->replot();
+  } catch (...) {
+    ui->text_label->setText("invalid parametr");
+  }
+
+  ////  N = (xEnd - xBegin) / h + 2;
+
+}
+
